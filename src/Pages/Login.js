0a3fb4a9fcf931ec4b110e485471fce835c8422d
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2022-10-18 23:02:30 -0700

    login page complete

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,44 +4,40 @@ import { useNavigate } from "react-router-dom";
 const Logger = (props) => {
     const [userName, setUserName] = useState("")
     const [password, setPassword] = useState("")
+
     const navigate = useNavigate()
     const redirectLoggedInUserFunction = () => {
         navigate("/home")
     }
-
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+
     return (
         <div className="contact-inputs-login" >
             <h1>Login Area</h1>
+            <input placeholder="username"
+                type="text"
+                value={props.userName}
+                onChange={(event) => {
+                    setUserName(event.target.value)
+                    console.log(userName)
 
-            <input placeholder="username" 
-            type="text" 
-            value={props.userName} 
-            onClick={(event) => {
-                setUserName(event.target.value)
-                console.log(userName)
-
-            }}></input>
+                }}></input>
             <br />
-            <input placeholder="password" 
-            type="text" 
-            value={props.password} 
-            onClick={(event) => {
+            <input placeholder="password"
+                type="text"
+                value={props.password}
+                onChange={(event) => {
                     setPassword(event.target.value)
                     console.log(password)
-            }}></input>
+                }}></input>
             <br />
             <button onClick={() => {
-                
-
-                    userName === "LakerLegend" &&
-                    password === "kobebryant" ?
-                    redirectLoggedInUserFunction() :
-                    alert("Please log in")
+                userName === "LakerLegend" &&
+                password === "kobebryant" ?
+                redirectLoggedInUserFunction() :
+                alert("Please log in")
 
                 setIsLoggedIn(!isLoggedIn)
-
-
 
             }}>Log In</button>
         </div>
